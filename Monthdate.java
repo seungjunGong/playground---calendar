09ecb58a418adcba1681a9aa8bd52c017c9d2b02
seungjunGong <jjw5741@naver.com>
2022-07-28 14:19:50 +0900

    class if문 제거

--- Monthdate.java
+++ Monthdate.java
@@ -1,12 +1,6 @@
 public class Monthdate {
     public static int date(int d){
-        int day = 0;
         int[] maxday = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        for(int i = 0; i < 12; i++){
-            if(d == (i+1)){
-                day = maxday[i];
-            }
-        }
-        return day;
+        return maxday[d-1];
     }
 }

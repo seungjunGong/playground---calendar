645979c635a6ff42d915b3aa269e64baa99ae4c7
seungjunGong <jjw5741@naver.com>
2022-07-28 15:43:58 +0900

    print monthdate

--- java_calendar.java
+++ java_calendar.java
@@ -2,22 +2,18 @@ import java.util.Scanner;
 
 public class java_calendar {
     public static void main(String[] args) {
-        System.out.println("반복횟수를 입력하세요.");
-        Scanner scan = new Scanner(System.in);
-        int count = scan.nextInt();
         String  PROMPT= ">";
-        
-        for(int i = 0; i < count; i++){
+        Scanner scan = new Scanner(System.in);
+        while(true){
             System.out.println("월을 입력하세요.");
             System.out.print(PROMPT);
             int num = scan.nextInt();
+            Printsamplecalendar.printcalender(num);
             if(num == -1){
                 break;
             }
-            System.out.println(num + "은" + Monthdate.date(num) + "까지 있습니다.");
         }
-        System.out.println("Have a nice day!");
-        Printsamplecalendar.printcalender();
+        System.out.println("Bye~");
         
         scan.close();
     }

790328a0d1208726a8d3b458d73a7ee21796fdf6
seungjunGong <jjw5741@naver.com>
2022-07-30 10:58:06 +0900

    윤년 포함 계산

--- Monthdate.java
+++ Monthdate.java
@@ -1,6 +1,6 @@
 public class Monthdate {
     public static int date(int d){
-        int[] maxday = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int[] maxday = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 29};
         return maxday[d-1];
     }
 }
--- Printcalendar.java
+++ Printcalendar.java
@@ -4,6 +4,11 @@ public class Printcalendar {
         System.out.println("일 월 화 수 목 금 토 일");
         System.out.println("--------------------");
         System.out.printf("    <<%4d년%3d월>>\n", year, month);
+        if(year % 4 == 0 || year % 400 == 0){
+            if(month == 2){
+                month = 13;
+            }
+        }
         for(int i = 1; i <= Monthdate.date(month); i++){
             System.out.printf("%3d", i);
             if(i % 7 == 0){
